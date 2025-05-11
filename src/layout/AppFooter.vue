5ad8c21943825376b838510b0f6cec36bb5f9cba
faisya12 <faifai.faisya@gmail.com>
2025-05-11 09:45:43 +0700

    logo changed

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="layout-footer">
-        SAKAI by
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
+        Budget Buddy by
+        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">BION</a>
     </div>
 </template>
